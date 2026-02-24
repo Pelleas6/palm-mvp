ba97fe4b9dd37b35c983f0353d1c29a4c16852c9
Pelleas6 <karimsoualem@gmail.com>
2026-02-24 15:03:10 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Lecture de Mains</h1>
+      <p>Votre analyse énergétique arrive bientôt.</p>
+    </main>
+  );
+}
