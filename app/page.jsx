fec98cfa825d72406c937ad392fa08fa9e654e86
Pelleas <karimsoualem@gmail.com>
2026-07-20 16:06:48 +0200

    feat: launch responsive WebGL news map

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import WorldPulseDashboard from "../components/WorldPulseDashboard";
-import { getWorldPulseDashboardPayload } from "../lib/world-pulse.js";
+import WorldMap from "../components/WorldMap";
+import DeferredWorldPulseDashboard from "../components/DeferredWorldPulseDashboard";
 
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const initialPayload = await getWorldPulseDashboardPayload();
-
+export default function Home() {
   return (
     <>
       <Header backLink={null} />
-      <WorldPulseDashboard initialPayload={initialPayload} />
+      <WorldMap />
+      <DeferredWorldPulseDashboard />
       <Footer />
     </>
   );
